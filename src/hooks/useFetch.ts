77a42d39ca8e999ab94be700e32742d974b97814
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-04-14 16:40:30 +0200

    Call useEffect with a dependency of url inside useFetch.ts

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -4,6 +4,8 @@ const useFetch = (url: string) => {
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+
+  useEffect(() => {}, [url]);
 };
 
 export default useFetch;
