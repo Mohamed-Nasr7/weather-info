0dd6e633bad642722ce5f8fa05e2d7d84fb49681
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-04-14 16:28:55 +0200

    Use the useState hook to create states named data, isLoading and error inside useFetch hook

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,5 +1,9 @@
 import { useState, useEffect } from 'react';
 
-const useFetch = (url: string) => {};
+const useFetch = (url: string) => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+};
 
 export default useFetch;
