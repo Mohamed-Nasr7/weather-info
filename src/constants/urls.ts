68b10f8855552d871ac7fe6c592c2248d0eb0dec
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-04-14 05:14:52 +0200

    Rename weather constant

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,2 +1,2 @@
-export const weather = 'https://api.openweathermap.org/data/2.5/weather';
+export const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
 export const countriesUrl = 'https://restcountries.com/v3.1/all';
