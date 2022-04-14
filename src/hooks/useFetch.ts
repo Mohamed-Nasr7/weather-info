5ac6e270797a057ac6c2f913c9aab0935ebc8868
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-04-14 20:14:26 +0200

    Set the data state in getData function with the resolved result

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 
 const useFetch = (url: string) => {
-  const [data, setData] = useState<object | object[] | null>(null);
+  const [data, setData] = useState<object | null>(null);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<any>(null);
 
@@ -9,6 +9,7 @@ const useFetch = (url: string) => {
     const getData = async () => {
       const response = await fetch(url);
       const data = await response.json();
+      setData(data);
     };
     getData();
   }, [url]);
