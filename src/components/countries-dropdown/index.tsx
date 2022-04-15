eb46551c021047505af31105ec087904c451eb88
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-04-15 03:21:25 +0200

    Call useFetch hook and destruct its returned properties

--- src/components/countries-dropdown/index.tsx
+++ src/components/countries-dropdown/index.tsx
@@ -2,6 +2,7 @@ import useFetch from '../../hooks/useFetch';
 import { countriesUrl } from '../../constants/urls';
 
 const CountriesDropdown = () => {
+  const { data, isLoading, error } = useFetch(countriesUrl);
   return <div></div>;
 };
 
