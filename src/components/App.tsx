d4574a5f0893eea0210ac91e1b402165e51cf54c
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-04-15 14:49:33 +0200

    Create a countryLatLong state in App.ts

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CountriesDropdown from './countries-dropdown';
 import WeatherInfo from './weather-info';
 
 const App = () => {
+  const [countryLatLong, setCountryLatLong] = useState([]);
+
   return (
     <div>
       <CountriesDropdown />
