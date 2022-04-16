14520e8daf55014d231fe29e156e1706aafba100
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-04-17 01:45:46 +0200

    Get the api key variable from the environment into weather-info/index.ts

--- src/components/weather-info/index.tsx
+++ src/components/weather-info/index.tsx
@@ -1,9 +1,11 @@
 import useFetch from '../../hooks/useFetch';
-import { weatherUrl as url } from '../../constants/apis';
+import { weatherUrl } from '../../constants/apis';
 
 type Props = {
   countryCoordinates: { [key: string]: number };
 };
+
+const apiKey = process.env.REACT_APP_API_KEY;
 
 const WeatherInfo = ({ countryCoordinates }: Props) => {
   return <div></div>;
