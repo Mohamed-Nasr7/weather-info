fa097bb7c96e14470ed19fff077bd16ce1eed16c
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-04-16 20:11:08 +0200

    Add a className to the wrapper div in App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,7 +6,7 @@ const App = () => {
   const [countryCoordinates, setCountryCoordinates] = useState<object>({});
 
   return (
-    <div>
+    <div className='app'>
       <CountriesDropdown setCountryCoordinates={setCountryCoordinates} />
       <WeatherInfo />
     </div>
