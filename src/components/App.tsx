e2e6f4e80efe8f49c2cda0363b0bb5d25c03df49
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-04-16 20:34:23 +0200

    Pass the state countryCoordinates to the WeatherInfo component props

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <div className='app'>
       <CountriesDropdown setCountryCoordinates={setCountryCoordinates} />
-      <WeatherInfo />
+      <WeatherInfo countryCoordinates={countryCoordinates} />
     </div>
   );
 };
