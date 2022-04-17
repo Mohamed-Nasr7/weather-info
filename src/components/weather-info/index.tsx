ddf9d4d29e17eb8dc150283d454aa39a7c184dad
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-04-17 02:44:41 +0200

    Add the jsx elements structure of the WeatherInfo component

--- src/components/weather-info/index.tsx
+++ src/components/weather-info/index.tsx
@@ -10,7 +10,17 @@ const apiKey = process.env.REACT_APP_API_KEY;
 const WeatherInfo = ({ countryCoordinates }: Props) => {
   const URL = `${weatherUrl}?lat=${countryCoordinates.lat}&lon=${countryCoordinates.long}&appid=${apiKey}`;
   const { data, isLoading, error } = useFetch(URL);
-  return <div></div>;
+  return (
+    <div>
+      <h2>Weather</h2>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
 };
 
 export default WeatherInfo;
