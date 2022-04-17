67153cc34102f7fa25dcae2d8a22182b44f1a592
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-04-17 17:36:09 +0200

    Check the data property to render the weather info jsx elements

--- src/components/weather-info/index.tsx
+++ src/components/weather-info/index.tsx
@@ -15,15 +15,17 @@ const WeatherInfo = ({ countryCoordinates }: Props) => {
   return isLoading ? (
     <p>Loading.. </p>
   ) : (
-    <div>
-      <h2>{country.name} Weather</h2>
-      <ul>
-        <li>{country.main.temp}</li>
-        <li>{country.main.pressure}</li>
-        <li>{country.main.humidity}</li>
-        <li>{country.wind.speed}</li>
-      </ul>
-    </div>
+    data && (
+      <div>
+        <h2>{country.name} Weather</h2>
+        <ul>
+          <li>{country.main.temp}</li>
+          <li>{country.main.pressure}</li>
+          <li>{country.main.humidity}</li>
+          <li>{country.wind.speed}</li>
+        </ul>
+      </div>
+    )
   );
 };
 
