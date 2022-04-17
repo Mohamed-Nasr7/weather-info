3c60f798d11e7c783daa5735ee41c22ea63912a8
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-04-17 02:23:06 +0200

    Call useFetch hook inside WeatherInfo component and destruct the returned object

--- src/components/weather-info/index.tsx
+++ src/components/weather-info/index.tsx
@@ -9,6 +9,7 @@ const apiKey = process.env.REACT_APP_API_KEY;
 
 const WeatherInfo = ({ countryCoordinates }: Props) => {
   const URL = `${weatherUrl}?lat=${countryCoordinates.lat}&lon=${countryCoordinates.long}&appid=${apiKey}`;
+  const { data, isLoading, error } = useFetch(URL);
   return <div></div>;
 };
 
