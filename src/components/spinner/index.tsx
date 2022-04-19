ad1eccb77a833ef1731e034127fe6b4efe2285a9
Mohamed Nasr <47702856+Mohamed-Nasr7@users.noreply.github.com>
2022-04-20 01:05:38 +0200

    Add the Spinner component jsx elements

--- src/components/spinner/index.tsx
+++ src/components/spinner/index.tsx
@@ -1,5 +1,13 @@
 const Spinner = () => {
-  return <div></div>;
+  return (
+    <div className='spinner'>
+      <div className='rect1'></div>
+      <div className='rect2'></div>
+      <div className='rect3'></div>
+      <div className='rect4'></div>
+      <div className='rect5'></div>
+    </div>
+  );
 };
 
 export default Spinner;
